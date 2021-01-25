cc601d06ee6f77ddb75efa6c28fcb8b340f793dd
TataProgrammer <73477872+TataProgrammer@users.noreply.github.com>
2021-01-25 11:00:38 +0300

    flower

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+
+let round = document.querySelector('.round');
+let stem = document.querySelector('.stem');
+let petal = document.querySelector('.petal');
+let petal1 = document.querySelector('.petal1');
+let petal2 = document.querySelector('.petal2');
+let petal3 = document.querySelector('.petal3');
+let petal4 = document.querySelector('.petal4');
+let petal5 = document.querySelector('.petal5');
+let petal6 = document.querySelector('.petal6');
+let petal7 = document.querySelector('.petal7');
+let centerleaf = document.querySelector('.centerleaf');
+let bottomleaf = document.querySelector('.bottomleaf');
+let body = document.querySelector('body');
+
+
+
+petal1.addEventListener('click', () =>{
+
+    body.style.background = 'red';
+
+    })
+
+    petal2.addEventListener('click', () =>{
+
+        body.style.background = 'paleturquoise';
+    
+        })   
+
+        petal3.addEventListener('click', () =>{
+
+            body.style.background = 'blueviolet';
+        
+            })   
+
+            petal4.addEventListener('click', () =>{
+
+                body.style.background = 'palegreen';
+            
+                })  
+
+                petal5.addEventListener('click', () =>{
+
+                    body.style.background = 'pink';
+                
+                    })  
+
+                    petal6.addEventListener('click', () =>{
+
+                        body.style.background = 'blue';
+                    
+                        })  
+
+                        petal7.addEventListener('click', () =>{
+
+                            body.style.background = 'peru';
+                        
+                            })  
+
+                  
+                                
+
+            
+        
+    
+
+
+              
+
+       
+
+
+
+
+
+
